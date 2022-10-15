05cf0facb1610f350508a25df094d4df2c85f13b
Abhinav Gupta <65679154+Abhinav250@users.noreply.github.com>
2022-10-15 22:49:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     while True:
         notifyme()
         notification.notify(
-            title = """Please Drink Water Now""",
+            title = """Please Drink Water Now!""",
             message = "1. Water Helps to Maximize Physical Performance" \
                   " 2. Hydration Has a Major Effect on Energy Levels and Brain Function" \
                   "3. Drinking Water May Help to Prevent and Treat Headaches" \
